a059b0538bd8202f1efac3259564eb45a70a4bf3
sebesvoj <vojta.sebesta@gmail.com>
2024-02-13 04:16:39 +0100

    vid5
    https://www.youtube.com/watch?v=43MeRuCrHbY&list=PLY3j36HMSHNX5JbYBrpN5ZFC6dbFBY2s_&index=5

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+add_action('init', function(){      //init is a hook that runs after WordPress has finished loading but before any headers are sent
 
-wp_enqueue_style('bootstrap', get_template_directory_uri().'/assets/css/bootstrap.min.css');
-//wp_enqueue_script('bootstrap', get_template_directory().'/assets/js/bootstrap.min.js', array('jquery'), false, true);
+    wp_enqueue_style('bootstrapcss', get_template_directory_uri().'/assets/css/bootstrap.min.css');
+    wp_enqueue_script('bootstrapjs', get_template_directory_uri().'/assets/js/bootstrap.min.js', ['jquery'], '', true); //true means the script will be placed at the bottom of the page
+
+});
+
--- index.php
+++ index.php
@@ -1,29 +1,33 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php bloginfo('description')?>">
+    <meta name="description" content="<?php bloginfo('description') ?>">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>  
 
     <?php
-        if ( have_posts()){
-            while ( have_posts()){
-                the_post();
-                ?>
-                <h1><?php the_title(); ?></h1>
-                <p><?php the_excerpt(); ?></p>
-                <hr>
-                <?php
-            }
+    get_template_part('templates/nav');
+
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+    ?>
+            <h1><?php the_title(); ?></h1>
+            <p><?php the_excerpt(); ?></p>
+            <hr>
+    <?php
         }
-        ?>  
+    }
+    ?>
 
     <?php wp_footer(); ?>
 </body>
+
 </html>
