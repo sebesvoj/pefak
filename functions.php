d3181a64c50a271901550efdad8da0b92e783346
sebesvoj <vojta.sebesta@gmail.com>
2024-02-13 07:15:06 +0100

    8

--- functions.php
+++ functions.php
@@ -2,8 +2,17 @@
 
 add_action('init', function(){      //init is a hook that runs after WordPress has finished loading but before any headers are sent
 
+    if (is_admin()) return; //if we are in the admin panel, we don't want to enqueue any styles or scripts
+
     wp_enqueue_style('bootstrapcss', get_template_directory_uri().'/assets/css/bootstrap.min.css');
     wp_enqueue_script('bootstrapjs', get_template_directory_uri().'/assets/js/bootstrap.min.js', ['jquery'], '', true); //true means the script will be placed at the bottom of the page
 
+});
+
+add_action('after_setup_theme', function(){
+    add_theme_support('widgets'); //this will enable the use of widgets in the theme
+    add_theme_support('menus'); //this will enable the use of menus in the theme
+    add_theme_support('post-thumbnails'); //this will enable the use of featured images in the theme
+    add_image_size('post-preview', 280, 180, false); //this will add a custom image size for the post preview
 });
 
--- templates/post.php
+++ templates/post.php
@@ -1,8 +1,11 @@
-<div class="card m-4 shadow border" style="max-width: 280px; min-width: 280px; min-height: 300px; max-height: 300px;">
-    <img src="<?= get_template_directory_uri() . '/assets/images/info.webp' ?>" class="card-img-top" alt="...">
+<div class="card m-4 shadow border rounded-3" style="max-width: 280px; min-width: 280px; height: 450px;">
+
+    <img src="<?php $image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large');
+                if ($image == true) echo $image[0];
+                else echo get_template_directory_uri() . '/assets/images/placeholder.png'; ?>" class="card-img-top rounded-top-3" alt="Logo oboru">
     <div class="card-body d-flex flex-column">
-        <h5 class="card-title"><?php the_title()?></h5>
-        <p class="card-text"><?= substr(get_the_excerpt(), 0, 100)?>..</p>
-        <a href="<?php the_permalink()?>" class="btn btn-primary mt-auto">Čti dál</a>
+        <h5 class="card-title"><?php the_title() ?></h5>
+        <p class="card-text"><?= substr(get_the_excerpt(), 0, 100) ?>..</p>
+        <a href="<?php the_permalink() ?>" class="btn btn-primary mt-auto">Čti dál</a>
     </div>
 </div>
